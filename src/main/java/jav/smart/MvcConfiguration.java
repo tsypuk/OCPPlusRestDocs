59231f8506a50eb4100bd2a1f111f701459b034b
Roman Tsypuk <tsypuk.rb@gmail.com>
2016-06-04 21:28:24 +0300

    added mvc

---
+++ src/main/java/jav/smart/MvcConfiguration.java
@@ -0,0 +1,30 @@
+package jav.smart;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by roman.tsypuk on 6/4/16.
+ */
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
---
+++ src/main/java/jav/smart/controllers/OCPWebController.java
@@ -0,0 +1,33 @@
+package jav.smart.controllers;
+
+import jav.smart.model.Question;
+import jav.smart.service.LoadOcpDumper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by roman.tsypuk on 6/4/16.
+ */
+@Controller
+public class OCPWebController {
+    @Autowired
+    private LoadOcpDumper loadOcpDump;
+
+    @RequestMapping("/greeting{id}")
+    public ModelAndView greeting(@RequestParam(value="id", required=false, defaultValue="1") long id, Model model) {
+//        model.addAttribute("name", loadOcpDump.loadQuestion(1).getText());
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("greeting");
+
+        mav.addObject("question", loadOcpDump.loadQuestion(id).getText());
+        mav.addObject("answers", loadOcpDump.loadQuestion(id).getAnswers());
+        return mav;
+    }
+}
